9d8c86fc259dbd22250096e13882bec606ff70ce
Craig <craigharrower02@hotmail.com>
2025-03-05 13:34:47 +0000

    fix: correct SVG path for MoonIcon component

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const ClockIcon = ({ size = 24 }) => (
 
 const MoonIcon = ({ size = 24 }) => (
   <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-    <path d="M12 3a6.364 6.364 0 0 0 9 9 9 9 0 1 1-9-9Z"/>
+    <path d="M12 3a6.364 6.364 0 0 0 9 9 9 0 1 1-9-9Z"/>
   </svg>
 );
 
